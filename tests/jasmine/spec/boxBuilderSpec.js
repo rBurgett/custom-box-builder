99c2e5140ceb8cd5ec97fdbf193e04fb52ee8094
rBurgett <ryan@burgettweb.net>
2014-09-26 20:56:07 -0400

    Added new tests. Updated boxBuilder

--- tests/jasmine/spec/boxBuilderSpec.js
+++ tests/jasmine/spec/boxBuilderSpec.js
@@ -10,6 +10,12 @@ describe("boxBuilder object", function() {
     it("should exist", function() {
         expect(boxBuilder).toBeTruthy();
     });
+    it("should have a default foam corner width", function() {
+        expect(boxBuilder.foamCornerWidth()).toBeTruthy();
+    });
+    it("should have a default cardboard width", function() {
+        expect(boxBuilder.cardboardWidth()).toBeTruthy();
+    });
     it("should have a length", function() {
         expect(boxBuilder.objectLength()).toEqual(24);
     });
@@ -18,5 +24,25 @@ describe("boxBuilder object", function() {
     });
     it("should have a height", function() {
         expect(boxBuilder.objectHeight()).toEqual(32.25);
+    });
+    describe("init method", function () {
+        it("should take an optional foamCornerWidth property", function () {
+            boxBuilder = BoxBuilder.create({
+                objectLength : 24,
+                objectWidth : 20.75,
+                objectHeight : 32.25,
+                foamCornerWidth : 0.75
+            });
+            expect(boxBuilder.foamCornerWidth()).toEqual(0.75);
+        });
+        it("should take an optional cardboardWidth property", function () {
+            boxBuilder = BoxBuilder.create({
+                objectLength : 24,
+                objectWidth : 20.75,
+                objectHeight : 32.25,
+                cardboardWidth : 0.125
+            });
+            expect(boxBuilder.cardboardWidth()).toEqual(0.125);
+        });
     });
 });
