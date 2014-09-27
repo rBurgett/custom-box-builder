bf02f4dd873a7fb42f9141e8287296a759a1d48f
rBurgett <ryan@burgettweb.net>
2014-09-27 15:58:29 -0400

    Added tests, added code

--- js/libs/boxBuilder.js
+++ js/libs/boxBuilder.js
@@ -4,6 +4,8 @@ var BoxBuilder = {
     _objectHeight : '',
     _foamCornerWidth : 1500,
     _cardboardWidth : 250,
+    _foldWidth: 250,
+    _foamCorners : true,
     objectLength : function() {
         return this._objectLength / 1000;
     },
@@ -25,7 +27,7 @@ var BoxBuilder = {
     init : function(d) {
         this._objectLength = d.objectLength * 1000 || '';
         this._objectWidth = d.objectWidth * 1000 || '';
-        this._objectHeight = d.objectHeight * 1000 || '';
+        this._objectHeight = d.objectHeight * 1000 || 5500;
         if (d.foamCornerWidth) {
             this._foamCornerWidth = d.foamCornerWidth * 1000;
         };
@@ -48,10 +50,31 @@ var BoxBuilder = {
                     return 'flat';
                 }
                 else {
-                    return 'skinny-flat';
+                    return 'skinny';
                 }
             }
         }
-        console.log(dims);
+    },
+    specs : function () {
+        var topLength;
+        var topWidth;
+        var bottomLength;
+        var bottomWidth;
+        var flapLength = 5500;
+        if (this.boxType() !== 'quad' && this._objectHeight > 5500) {
+            flapLength = this._objectHeight;
+        }
+        if (this._foamCorners) {
+            topLength = this._objectLength + (2 * this._foamCornerWidth) + (2 * this._foldWidth) + (2 * this._cardboardWidth);
+            topWidth = this._objectWidth + (2 * this._foamCornerWidth) + (2 * this._foldWidth) + (2 * this._cardboardWidth);
+            return {
+                'top' : {
+                    'flapLength' : flapLength,
+                    'length' : topLength,
+                    'width' : topWidth,
+                    'totalLength' : 2 * this._foldWidth
+                }
+            }
+        }
     }
 };
--- tests/jasmine/spec/boxBuilderSpec.js
+++ tests/jasmine/spec/boxBuilderSpec.js
@@ -16,14 +16,20 @@ describe("boxBuilder object", function() {
     it("should have a default cardboard width", function() {
         expect(boxBuilder.cardboardWidth()).toBeTruthy();
     });
+    it("should have a cardboard fold width", function() {
+        expect(boxBuilder._foldWidth).toBeTruthy();
+    });
+    it("should have a foam corners property set to true", function() {
+        expect(boxBuilder._foamCorners).toBeTruthy();
+    });
     it("should have a length", function() {
-        expect(boxBuilder.objectLength()).toEqual(24);
+        expect(boxBuilder.objectLength()).toBeTruthy();
     });
     it("should have a width", function() {
-        expect(boxBuilder.objectWidth()).toEqual(20.75);
+        expect(boxBuilder.objectWidth()).toBeTruthy();
     });
     it("should have a height", function() {
-        expect(boxBuilder.objectHeight()).toEqual(32.25);
+        expect(boxBuilder.objectHeight()).toBeTruthy();
     });
     describe("init method", function () {
         it("should take an optional foamCornerWidth property", function () {
@@ -46,7 +52,7 @@ describe("boxBuilder object", function() {
         });
     });
     describe("boxType method", function () {
-        it("should return 'flat' if the smallest dimension is less than 9 and the other dimensions are each more than twice as long as the smallest dimension", function () {
+        it("should return 'flat' if the smallest dimension is less than 9 inches and the other dimensions are each more than twice as long as the smallest dimension", function () {
             boxBuilder = BoxBuilder.create({
                 objectLength : 24,
                 objectWidth : 20,
@@ -54,13 +60,26 @@ describe("boxBuilder object", function() {
             });
             expect(boxBuilder.boxType()).toBe("flat");
         });
-        it("should return 'skinny-flat' if the smallest dimension is less than 9 and one of the other two dimensions is less than twice as long as the smallest dimension", function () {
+        it("should return 'skinny' if the smallest dimension is less than 9 inches and one of the other two dimensions is less than twice as long as the smallest dimension", function () {
             boxBuilder = BoxBuilder.create({
                 objectLength : 24,
                 objectWidth : 10,
                 objectHeight : 6,
             });
-            expect(boxBuilder.boxType()).toBe("skinny-flat");
+            expect(boxBuilder.boxType()).toBe("skinny");
+        });
+    });
+    describe("specs method", function () {
+        it("should return an object", function () {
+            expect(boxBuilder.specs()).toBeTruthy();
+        });
+        describe("if the boxType is flat, the object", function () {
+            it("should have a top", function () {
+                expect(boxBuilder.specs().top).toBeTruthy();
+            });
+//            it("should have a bottom", function () {
+//                expect(boxBuilder.specs().bottom).toBeTruthy();
+//            });
         });
     });
 });
