1224082288e481a6ca3f7c5ab5c58fe284e213c3
rBurgett <ryan@burgettweb.net>
2014-09-26 20:55:05 -0400

    Added new tests

--- js/libs/boxBuilder.js
+++ js/libs/boxBuilder.js
@@ -2,22 +2,36 @@ var BoxBuilder = {
     _objectLength : '',
     _objectWidth : '',
     _objectHeight : '',
+    _foamCornerWidth : 1500,
+    _cardboardWidth : 250,
     objectLength : function() {
-        return this._objectLength;
+        return this._objectLength / 1000;
     },
     objectWidth : function() {
-        return this._objectWidth;
+        return this._objectWidth / 1000;
     },
     objectHeight : function() {
-        return this._objectHeight;
+        return this._objectHeight / 1000;
+    },
+    foamCornerWidth : function() {
+        return this._foamCornerWidth / 1000;
+    },
+    cardboardWidth : function() {
+        return this._cardboardWidth / 1000;
     },
     create : function(d) {
         return Object.create(BoxBuilder).init(d);
     },
     init : function(d) {
-        this._objectLength = d.objectLength || '';
-        this._objectWidth = d.objectWidth || '';
-        this._objectHeight = d.objectHeight || '';
+        this._objectLength = d.objectLength * 1000 || '';
+        this._objectWidth = d.objectWidth * 1000 || '';
+        this._objectHeight = d.objectHeight * 1000 || '';
+        if (d.foamCornerWidth) {
+            this._foamCornerWidth = d.foamCornerWidth * 1000;
+        };
+        if (d.cardboardWidth) {
+            this._cardboardWidth = d.cardboardWidth * 1000;
+        };
         return this;
     },
 };
